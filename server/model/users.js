877925c46673a48f3b874a065a082a7f81691c2c
Gfreedoms <freedomgemmar1@gmail.com>
2019-03-08 11:39:48 +0300

    add user model [Delivers#164490479]

---
+++ server/model/users.js
@@ -0,0 +1,33 @@
+const users = [
+	{
+		id: 1,
+		firstname: 'Freedom',
+		lastname: 'Gemmar',
+		email: 'free@test.com',
+		password: 'jan23242',
+		confirmpassword: 'jan23242',
+		isAdmin: 'true',
+	},
+
+	{
+		id: 2,
+		firstname: 'Acidri',
+		lastname: 'Ian',
+		email: 'acidriian@app.com',
+		password: 'jojo2144',
+		confirmpassword: 'jojo2144',
+		isAdmin: 'false',
+	},
+
+	{
+		id: 2,
+		firstname: 'Acidri',
+		lastname: 'Ian',
+		email: 'acidriian@app.com',
+		password: 'status1323',
+		confirmpassword: 'status1324',
+		isAdmin: 'false',
+	},
+];
+
+export default users;
